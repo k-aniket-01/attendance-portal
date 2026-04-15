de05538c8737b4dd9ac8d4e60b89b8b649ee3c21
k-aniket-01 <khomaneaniket9420@gmail.com>
2026-04-15 15:45:30 +0530

    [fix] urls

--- attendance_backend/attendance/urls.py
+++ attendance_backend/attendance/urls.py
@@ -1,9 +1,10 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import path
 from .views import check_in, check_out, timesheet, apply_leave, get_leaves
 
 urlpatterns = [
-    path('api/login/', TokenObtainPairView.as_view()),
+    path('api/login/', csrf_exempt(TokenObtainPairView.as_view())), 
     path('api/checkin/', check_in),
     path('api/checkout/', check_out),
     path('api/timesheet/', timesheet),
