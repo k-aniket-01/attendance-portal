ceca0cb34489c645a0fb52e82c0360a285f6f119
k-aniket-01 <khomaneaniket9420@gmail.com>
2026-04-16 12:16:01 +0530

    [add] admin panel models

--- attendance_BE/core/models.py
+++ attendance_BE/core/models.py
@@ -16,13 +16,17 @@ class Leave(models.Model):
         ('SICK', 'Sick'),
         ('CASUAL', 'Casual'),
     )
-
+    STATUS_TYPE = (
+        ('PENDING', 'Pending'),
+        ('APPROVED', 'Approved'),
+        ('REJECTED', 'Rejected')
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
     leave_type = models.CharField(max_length=10, choices=LEAVE_TYPES)
     reason = models.TextField()
-    status = models.CharField(max_length=10, default='PENDING')
+    status = models.CharField(max_length=10,choices=STATUS_TYPE, default='PENDING')
 
     def __str__(self):
         return f"{self.user.username} - {self.status}"
